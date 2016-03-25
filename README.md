b61beaf96376a3a09353f1171f0e8c1bc8957c5e
Troy Schumaker <troy.schumaker@motili.com>
2016-03-25 11:31:14 -0600

    initial samples

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Work-Samples-NodeJS
+
+This directory contains the following sample work:
+
+1. File named: "WorkOrderNewComments.js"
+
+Description:  This is a backend NodeJS snippet that is utilizing the async-waterfall package.  This snippet gets
+called from the workflow engine, and does the following:
+
+  query() function - tries to retrieve from Mongo a workorder document.  If it can't find one it logs the error and exists.
+  If it CAN find the workorder document then it passes it on to the sendEmail function.
+
+  sendEmail() function - This retrieves email information (to, cc. etc. ) in addition to the last comment applied to this workorder
+  and attempts to send a notification email out to the recipient. Again, if it fails, it handles the logging of that error, else
+  it completes and finishes successfully.
+
+   
+-Troy Schumaker
---
+++ WorkOrderNewComments.js
@@ -0,0 +1,67 @@
+module.exports = (function () {
+    var Notifications = require("../Notifications");
+    var async         = require("async");
+    var moment        = require("moment-timezone");
+
+    return {
+        create: function () {
+            return new Object({
+                processWorkflow: function (wf, item, callback) {
+                    var models = this.app.models;
+
+                    async.waterfall([
+                        function query(callback) {
+                            models.WorkOrder.loadOne({
+                                where:   {_id: item.mongoId},
+                            }, function (error, workorder) {
+                                if (workorder) callback(null, workorder);
+                                else callback({msg: "No workorder found with id " + item.mongoId});
+                            });
+                        },
+
+                        function sendEmail(workorder, callback) {
+                            var to = models.WorkOrder.resolveNotificationEmail(workorder);
+                            var cc = models.WorkOrder.resolveNotificationCCEmail(workorder);
+                            var tz = models.WorkOrder.resolveTimeZone(workorder)
+                            if (tz == null) {
+                                tz = "US/EASTERN";  // defaulting to eastern time zone
+                            }                
+                            
+                            var lastComment = workorder.comments.slice(-1)[0];
+                            console.log("WorkorderNewComments::checking last comment for workorder#: " + workorder._id);                            
+                            if ( lastComment ) {  //  normal workflow would have this setup as: lastComment.type == "motili"
+                                lastComment.formattedDate = moment(lastComment.date).tz(tz).format("MMM Do YYYY, h:mm a"); // user's time zone
+                                //console.log("WorkorderNewCommentsToMotili::lastComment is: " + lastComment + " and formattedDate is: " + lastComment.formattedDate);
+                            }
+
+                            workorder.lastcomment = lastComment;
+
+                            // Add in the well-defined URL to the job...
+                            var aSystem        = wf.config.systemInfo;
+                            workorder.urlToJob = aSystem.contractorPortal + "/jobdetail/" + workorder._id;
+
+                            var data = {
+                                workorder: workorder,
+                                user:      workorder.assignedUser,
+                                company:   workorder.assignedCompany,
+                                comment:   item.jsonData && item.jsonData.comment ? item.jsonData.comment : "No comment",
+                                system:    wf.config.systemInfo
+                            };
+
+                            Notifications.sendEmailNotification("workorder_new_comments_email_to_contractor", to, cc, tz, data, function (error, status) {
+                                if (error) {
+                                    console.log("WorkOrderNewcomments::error in sendEmailNotification, it was: " + error);
+                                    callback({msg: "WorkOrderNewcomments::error in sendEmailNotification, it was: " + error});                                    
+                                }
+
+                                callback(null, workorder);
+                            });
+                        }
+                    ], function (error, workorder) {
+                        callback(error)
+                    });
+                }
+            });
+        }
+    };
+})();
